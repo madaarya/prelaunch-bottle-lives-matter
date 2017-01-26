1266ddd80354db3b0cfc968e55d3db23c30491a6
Mada Aryakusumah <mada@41studio.com>
2017-01-26 11:15:47 +0700

    change favicon and url prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,11 +60,11 @@ Prelaunchr::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-  config.action_mailer.default_url_options = "http://138.197.68.79"
+  config.action_mailer.default_url_options = "http://prelauncher.bottlelivesmatter.us"
 
   config.action_mailer.delivery_method = :smtp
 
-  config.action_mailer.asset_host = "http://138.197.68.79"
+  config.action_mailer.asset_host = "http://prelauncher.bottlelivesmatter.us"
 
   config.action_mailer.smtp_settings = {
     :address => "smtp.sendgrid.net",
